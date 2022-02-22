93f7b6577ac87b0475b7dcb45ac7836e71ac1395
Pavel Harmaza <pavelharmaza@gmail.com>
2022-02-22 15:09:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const obj = {};
+  bracketsConfig.map((el) => (obj[el[0]] = el[1]));
+
+  const allBrackets = [];
+
+  for (let one of str) {
+    const lastOne = allBrackets[allBrackets.length - 1];
+    if (one in obj) {
+      obj[one] === one && one === lastOne
+        ? allBrackets.pop()
+        : allBrackets.push(one);
+    } else {
+      const last = obj[lastOne];
+      if (last === one) {
+        allBrackets.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return allBrackets.length === 0;
+};
